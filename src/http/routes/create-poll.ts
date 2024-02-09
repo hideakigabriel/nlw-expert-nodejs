b72e43fa34d8c011b8589284a08e2ee60a50e7ac
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-02-08 23:45:10 -0300

    feat: crate poll with options register in database

---
+++ src/http/routes/create-poll.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import { prisma } from "../../lib/prisma";
+import { FastifyInstance } from "fastify/types/instance";
+
+export async function createPoll(app: FastifyInstance) {
+  app.post("/polls", async (request, reply) => {
+    const createPollBody = z.object({
+      title: z.string(),
+      options: z.array(z.string()),
+    });
+
+    const { title, options } = createPollBody.parse(request.body);
+
+    const poll = await prisma.poll.create({
+      data: {
+        title,
+      },
+    });
+
+    await Promise.all(
+      options.map((option) => {
+        return prisma.pollOption.create({
+          data: {
+            title: option,
+            pollId: poll.id,
+          },
+        });
+      })
+    );
+
+    return reply.status(201).send({
+      pollId: poll.id,
+    });
+  });
+}
