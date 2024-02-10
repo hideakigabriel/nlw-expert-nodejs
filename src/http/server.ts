b1fc53824574e0cce3eee901d72edbbc8baf8fa9
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-02-10 02:14:56 -0300

    feat: create vote poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,13 +1,20 @@
-import fastify from "fastify"
-import { createPoll } from "./routes/create-poll"
-import { getPoll } from "./routes/get-poll"
+import fastify from "fastify";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnePoll } from "./routes/vote-one-poll";
+import cookie from "@fastify/cookie";
 
-const app = fastify()
+const app = fastify();
 
-app.register(createPoll)
+app.register(cookie, {
+  secret: "polls-app-nlw-hds", // for cookies signature
+  hook: "onRequest", // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
+});
 
-app.register(getPoll)
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnePoll);
 
-app.listen({port: 3333}).then(() => {
-  console.log(`HTTP server running!`)
-})
+app.listen({ port: 3333 }).then(() => {
+  console.log(`HTTP server running!`);
+});
